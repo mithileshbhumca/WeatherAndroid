5f40b159104c60af5d7fc3099efbe49800fae4b2
M3096_csx <mithilesh_kumar@csx.com>
2025-05-20 14:05:43 +0530

    upgraded gradle as per latest android studio april 25 release (Meerkat Feature Drop | 2024.3.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,18 @@ plugins {
     alias(libs.plugins.navigation.safeargs)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.jetbrains.kotlin.ksp)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.example.weatherforecast"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.weatherforecast"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,34 +34,37 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField ("boolean", "ENABLE_HTTP_LOGS", "true")
+            buildConfigField("boolean", "ENABLE_HTTP_LOGS", "true")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
-//    packaging {
-//        resources {
-//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-//        }
-//    }
     buildFeatures {
         buildConfig = true
     }
     buildFeatures {
         viewBinding = true
     }
-
+//    packaging {
+//        jniLibs {
+//            pickFirsts.add("META-INF/*")
+//        }
+//        resources {
+//            pickFirsts.add("META-INF/*")
+//        }
+//    }
 }
 
 dependencies {
